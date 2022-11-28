51917dce818517394510a26b54e526fb50eeb035
ClaudioFritzen <sdfritzen96@gmail.com>
2022-11-28 18:34:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var modulo = require("./modulo")
+
+if(modulo.somar(1,2) === 3){
+    console.log("Funcionou")
+}
+
+if(modulo.vezes(5,5) === 25){
+    console.log("5x5=25")
+}
---
+++ modulo.js
@@ -0,0 +1,9 @@
+function somar(valor1,valor2){
+    return valor1+valor2
+}
+
+function vezes(valor1, valor2) {
+    return valor1 * valor2  
+}
+
+module.exports = { somar, vezes}
